f8146a4b656a7b3fde1d7c68bcb3be42c0e1887a
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-08-10 10:54:46 -0400

    stuff

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,9 +4,14 @@ import '../css/Background.css';
 class Background extends Component{
   render(){
     return(
-      <div className="back">
-        <div className="banner">EMERGENCY DOC!</div>
-        <p className="pTag">Find the nearest public hospital located near you.</p>
+      <div className="full-banner">
+        <div className="flex-banner">
+          <div className="image"></div>
+          <div className="back">
+            <div className="banner">EMERGENCY DOC!</div>
+            <p className="pTag">Find the nearest public hospital located near you.</p>
+          </div>
+        </div>
       </div>
     )
   }
